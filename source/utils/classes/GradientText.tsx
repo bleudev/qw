60040ccb7e1abdbaa3574a5183b62a19cefec50d
bleudev <aitiiigg1@gmail.com>
2025-05-08 15:35:51 +0000

    Add ability to use 1 or 3+ colors in gradient

--- source/utils/classes/GradientText.tsx
+++ source/utils/classes/GradientText.tsx
@@ -8,24 +8,39 @@ type GradientTextProps = {
 };
 
 const GradientText: FunctionComponent<PropsWithChildren<GradientTextProps>> = ({colors, children: content}) => {
-    if (Array.isArray(content)) content = content.join('')
-    
-    if (typeof content == "string") {
-        const len = content.length;
-        return (
-            <Box>
-                {gradient_array(colors[0] || '#000000', colors[colors.length - 1] || '#000000', len).map(
-                    (v, i) => (
-                        <Text color={v}>
-                            {content[i]}
-                        </Text>
-                    )
-                )}
-            </Box>
-        )
+  if (Array.isArray(content)) content = content.join('')
+
+  if (typeof content == "string") {
+    if (typeof colors == "string")
+      colors = [colors];
+    const con_len = content.length;
+    const col_len = colors.length;
+
+    if (col_len == 1) {
+      var grarray: string[] = Array(con_len).fill(colors[0]);
     }
-    
-    return content;
+    else {
+      var grarray = gradient_array(colors[0] || '#000000', colors[1] || '#000000', Math.ceil(con_len / (col_len - 1)));
+  
+      for (let i = 1; i < colors.length - 1; i++) {
+        grarray.push(...gradient_array(colors[i] || '#000000', colors[i+1] || '#000000', Math.ceil(con_len / (col_len - 1))).slice(1))
+      }
+    }
+
+    return (
+      <Box>
+        {grarray.map(
+            (v, i) => (
+                <Text color={v}>
+                    {content[i]}
+                </Text>
+            )
+        )}
+      </Box>
+    );
+  }
+
+  return content;
 }
 
 export default GradientText;
